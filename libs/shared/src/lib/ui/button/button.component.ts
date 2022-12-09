41ffa239284880b06f21552e08569b7da2e26e22
joe <>
2022-12-09 19:55:49 +0800

    完成 lib-button 程式撰寫

--- libs/shared/src/lib/ui/button/button.component.ts
+++ libs/shared/src/lib/ui/button/button.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'lib-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+
+  @ViewChild('btn', { static: true }) button!: ElementRef;
+
+  @Input('class') set class(value: string) {
+    const classList = value.split(' ');
+    setTimeout(() => {
+      const formFieldClassList = this.button.nativeElement.classList;
+      classList.forEach((className) => {
+        formFieldClassList.add(className);
+      });
+    });
+  }
+
+  @Output() buttonClick = new EventEmitter<any>();
 
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+
+  addFavorite(){
+    console.log('add....');
+  }
+
 }
