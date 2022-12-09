1b843d16851e265ee0180a2f73343c05cde10ae2
joe <>
2022-12-09 19:24:09 +0800

    建立一個 lib 級的 空 sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from 'libs/shared/src/lib/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 import { LayoutComponent } from './layout/layout.component';
@@ -22,7 +25,8 @@ import { HeaderComponent } from './layout/header/header.component';
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
